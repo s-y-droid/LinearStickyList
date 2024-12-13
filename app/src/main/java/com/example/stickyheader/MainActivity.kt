9dedb473a851a10e899feeb322d19ae5dd877fc1
sydroid <s-yao@clinks.jp>
2024-12-13 10:46:58 +0900

    Add usage comment

--- app/src/main/java/com/example/stickyheader/MainActivity.kt
+++ app/src/main/java/com/example/stickyheader/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.sydroid.android.linearstickylist.LinearStickyListFragment
 import com.example.stickyheader.databinding.ActivityMainBinding
+import com.sydroid.android.linearstickylist.LinearStickyListScrollbarOptions
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,8 +17,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         savedInstanceState ?: run {
+            // Note #1
+            // Place the LinearStickyListFragment and call the setup() method.
+            // In the setup() method, specify the Fragments for all cells that make up the LinearStickyListFragment.
+            // (This Fragment is referred to as the "Cell Fragment" here.)
+            //
+            // Do not hold onto Cell Fragment in the caller, because it will be recreated.
+            // See Note #2 for information on how to safely retrieve the current Cell Fragment.
             getLinearStickyListFragment().setup(
-                listOf(
+                list = listOf(
                     P1CellFragment.newInstance(),
                     P2CellFragment.newInstance(patternNo = 0),
                     P3CellFragment.newInstance(),
@@ -25,7 +33,35 @@ class MainActivity : AppCompatActivity() {
                     P2CellFragment.newInstance(patternNo = 1),
                     P1CellFragment.newInstance(),
                     P5CellFragment.newInstance(),
-                )
+                ),
+                // Note #5 (Optional)
+                // Specify this if you want to change the design or functionality of the scrollbar.
+                // The default scrollbar has a design similar to ScrollView.
+                // The parameters are as follows:
+                //
+                // val isShowScrollbar : Boolean = true
+                //     ... true if scrollbars are displayed on screen
+                //
+                // val widthDp : Float = 4.0f
+                //     ... Only valid when isShowScrollbar=true.
+                //         Scrollbar width (unit: Dp)
+                //
+                // val drawableResId : Int = R.drawable.linear_sticky_list_defalut_scrollbar,
+                //     ... Only valid when isShowScrollbar=true.
+                //         The resource ID of the drawable to apply to the scrollbar.
+                //
+                // val isFadeOut : Boolean = true,
+                //     ... Only valid when isShowScrollbar=true.
+                //         Whether to hide the scrollbars if no scrolling occurs for a while.
+                //
+                // val isFadeOutAlphaAnimationTimeMs : Long = 600L,
+                //     ... Only valid when isFadeOut=true.
+                //         Time for the scrollbar to fade out (Unit: ms)
+                //
+                // val isFadeOutInactivityTimeMs : Long = 1200L
+                //     ... Only valid when isFadeOut=true.
+                //         The amount of inactivity time required for the scrollbar to fade out. (Unit: ms)
+                scrollbarOptions = LinearStickyListScrollbarOptions()
             )
         }
 
@@ -45,6 +81,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // Note #2
+    //　To safely get the current Cell Fragment, use the getFragmentByIdx() method of LinearStickyListFragment.
     private fun getCellFragment(idx: Int) =
         getLinearStickyListFragment().getFragmentByIdx(idx)
 
--- app/src/main/java/com/example/stickyheader/P1CellFragment.kt
+++ app/src/main/java/com/example/stickyheader/P1CellFragment.kt
@@ -8,6 +8,22 @@ import androidx.core.os.bundleOf
 import com.example.stickyheader.databinding.P1CellFragmentBinding
 import com.sydroid.android.linearstickylist.LinearStickyListCellFragmentBase
 
+// Note #3
+// The cell fragment inherits from LinearStickyListCellFragmentBase, which requires some overrides:
+//
+// 1. (Required) fun isStickyHeader(): Boolean
+//    ... Specify true if you want this Cell Fragment to act as a StickyHeader.
+//
+// 2. (Optional) fun onDistanceFromDisplayArea(isOnScreen: Boolean, distancePx: Float)
+//    ... The following information is called back:
+//
+//        isOnScreen : Whether the cell is visible on screen or not
+//        distancePx :　If isOnScreen is false, the distance away from the screen (unit: Px).
+//
+//        a callback will be made to tell you how far it is from the screen.
+//        Its purpose is to stop the animation of off-screen Cell or to free memory, etc.
+//        Please override if you need it.
+
 class P1CellFragment : LinearStickyListCellFragmentBase() {
 
     companion object {
@@ -35,6 +51,10 @@ class P1CellFragment : LinearStickyListCellFragmentBase() {
             binding.btn6, binding.btn7, binding.btn8, binding.btn9, binding.btn10
         ).forEachIndexed { idx, v ->
             v.setOnClickListener {
+                // Note #4
+                // If you want to pass event to the parent, use the setFragmentResult() method of LinearStickyListCellFragmentBase.
+                // This is recommended as using callbacks will not maintain references when the Cell Fragment is regenerated.
+                // (In this sample, the event "P1BtnClicked" is received by MainActivity.)
                 setFragmentResult("P1BtnClicked", bundleOf("idx" to idx + 1))
             }
         }
